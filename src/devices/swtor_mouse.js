7a1ee9bdaeca454506380d677cbd421fd701df19
Voice <53106311+TheodoreBrinkman@users.noreply.github.com>
2023-02-13 22:44:12 -0500

    Initial swag at device support
    Initial swag at config to support SWTOR Gaming Mouse V2.

---
+++ src/devices/swtor_mouse.js
@@ -0,0 +1,9 @@
+{
+    "name": "Razer SWTOR Mouse V2",
+    "productId": "0x002a",
+    "mainType": "mouse",
+    "image": "https://assets.razerzone.com/eeimages/support/products/185/star.wars.razer.mouse.jpg",
+    "features": []
+    "featuresMissing": ["none"]
+    "featuresConfig": []
+}
